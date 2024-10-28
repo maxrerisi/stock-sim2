fd87a68b9d8da8bd27a22458bef494bfa5b9c6ff
Max R <maxrerisi+git@gmail.com>
2024-10-27 22:15:25 -0400

    asd

--- current_price.py
+++
@@ -1,9 +0,0 @@
-import yfinance as yf
-
-
-def current_price(ticker):
-    ticker = yf.Ticker(ticker)
-    return ticker.history(period="1d")['Close'].iloc[-1]
-
-
-print(current_price("MSFT"))
--- get_date.py
+++
@@ -1,9 +0,0 @@
-import arrow
-
-
-def get_date():
-    return arrow.utcnow()
-
-
-def date_to_vis(date):
-    return date.humanize()
